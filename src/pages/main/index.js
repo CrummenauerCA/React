79f460c5b42d2ade34ffe82d132457c486d827ca
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-03 03:49:16 -0200

    Colocando e estilizando o disabled nos botões para avançar e voltar...

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -35,7 +35,7 @@ export default class Main extends Component {
 
     render() {
 
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state;
 
         return (
             <div className="product-list">
@@ -47,8 +47,8 @@ export default class Main extends Component {
                     </article>
                 ))}
                 <div className="actions">
-                    <button onClick={this.prevPage}>Anterior</button>
-                    <button onClick={this.nextPage}>Próxima</button>
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
                 </div>
             </div>
         );
