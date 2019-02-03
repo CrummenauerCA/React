6ceb909a994e4bb70b6a4c77d4c8e640bece2b2a
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-03 02:25:18 -0200

    Criando o header...

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">JShunt</header>
+);
+
+// const Header = () => <header id="main-header">JShunt</header>;
+export default Header;
+
+// class Header extends Component {
+//     render() {
+//         return <h1>Hello</h1>
+//     }
+// }
