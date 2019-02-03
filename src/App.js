f3e064623fcc8856cd0b54ba2f07af8bff36e964
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-03 02:35:28 -0200

    Adicionando app e main...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './styles.css';
 import Header from './components/Header';
+import Main from './pages/main';
 
 const App = () => (
     <div className="App">
-        <Header/>
-        <h1>Hello Cezar</h1>
+        <Header />
+        <Main />
 
     </div>
 );
