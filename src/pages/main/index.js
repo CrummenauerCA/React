56c1e226ba568f0946b3a7f2af0bdfb889a7b8d7
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-03 02:56:27 -0200

    Exibindo contagem de produtos...

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+    state = {
+        products: [],
+    }
+
     componentDidMount() {
         this.loadProducts();
     }
 
     loadProducts = async () => {
         const response = await api.get('/products');
-
-        console.log(response.data.docs);
+        this.setState({ products: response.data.docs });
     };
 
     render() {
-        return <h1>Hello Rocketseat</h1>
+        return <h1>Contagem de produtos: {this.state.products.length}</h1>
     }
 }
